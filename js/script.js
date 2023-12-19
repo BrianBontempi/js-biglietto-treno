8487555364374551adccf60bfa4bd33a61cdbbbe
BrianBontempi <brian.bont54@gmail.com>
2023-12-19 16:47:23 +0100

    add base variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("JS OK")
+
+// ! recupero elemento
+
+const element = document.getElementById("final-price");
+
+// ! variabile costo per km
+
+const priceKm = 0.21;
+
+// ! messaggio prezzo finale
+
+const priceMessage = "Il costo del tuo biglietto è: €";
+
+// ! eventuale sconto
+
+let sconto = null;
